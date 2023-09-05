0fa26e9af36cf177a887dc7b074746ec711f324b
Your Name <jamesdstacey2@gmail.com>
2023-09-05 21:21:41 +1000

    updated app.ts

--- app.ts
+++ app.ts
@@ -1,2 +1,11 @@
-let message: string = "Hello World!"
-console.log(message)
+// Exercise 1
+// let message: string = "Hello World!"
+// console.log(message)
+
+// Exercise 2
+let message: string = 'Hello, World!';
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading to the document
+document.body.appendChild(heading);
