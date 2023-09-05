8d620a181c8f37127757824cd22e236b31adb49e
Your Name <jamesdstacey2@gmail.com>
2023-09-05 22:17:11 +1000

    Added Bobby Hadz export tip to app.ts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+"use strict";
 // Exercise 1
 // let message: string = "Hello World!"
 // console.log(message)
+Object.defineProperty(exports, "__esModule", { value: true });
 // Exercise 2
 var message = 'Hello, World!';
 // create a new heading 1 element
@@ -8,3 +10,6 @@ var heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading to the document
 document.body.appendChild(heading);
+// Exercise on Understanding Type Annotations
+var counter;
+counter = 5;
--- app.ts
+++ app.ts
@@ -9,3 +9,9 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 // add the heading to the document
 document.body.appendChild(heading);
+
+// Exercise on Understanding Type Annotations
+let counter: number
+counter = 5
+
+export {}; // tip from Bobby Hadz to avoid 'Cannot redeclare block-scoped variable in TypeScript'
